eb5d8e6e3e7acba242f8eaef6238722be417d9a4
konstantin <kostia505@gmail.com>
2025-02-07 21:49:19 +0100

    crud test

--- main.go
+++ main.go
@@ -1,3 +1,72 @@
 package main
 
-func main() {}
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Book struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	title := vars["title"]
+
+	book := Book{Title: title}
+	json.NewDecoder(r.Body).Decode(&book)
+
+	fmt.Fprintf(w, "Book created: %+v\n", book)
+}
+
+func ReadBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	title := vars["title"]
+
+	book := Book{Title: title, Author: "Unknown"}
+
+	json.NewEncoder(w).Encode(book)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	title := vars["title"]
+
+	book := Book{Title: title}
+	json.NewDecoder(r.Body).Decode(&book)
+
+	fmt.Fprintf(w, "Book updated: %+v\n", book)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	title := vars["title"]
+
+	fmt.Fprintf(w, "Book deleted %s\n", title)
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	//+params
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	})
+
+	//crud
+	r.HandleFunc("/books/{title}", CreateBook).Methods("POST")
+	r.HandleFunc("/books/{title}", ReadBook).Methods("GET")
+	r.HandleFunc("/books/{title}", UpdateBook).Methods("PUT")
+	r.HandleFunc("/books/{title}", DeleteBook).Methods("DELETE")
+
+	//server run
+	http.ListenAndServe(":8080", r)
+}
